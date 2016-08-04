1af3de1feae137ddacb762e73ad842835ab52a9a
Ashish Baghudana <ashish.baghudana26@gmail.com>
2016-08-04 17:24:22 +0530

    Basic structure of code

--- src/ScoreBoard.java
+++ src/ScoreBoard.java
@@ -1,4 +1,29 @@
 
 public class ScoreBoard {
-
+	private int[] A;
+	private int[] B;
+	private final int SETS;
+	
+	private final int SETS_TO_WIN;
+	private final int MIN_GAMES_TO_WIN = 6;
+	private final int MIN_POINTS_TO_WIN = 4;
+	
+	public ScoreBoard(int sets) {
+		this.SETS = sets;
+		this.SETS_TO_WIN = this.SETS / 2 + 1;
+		A = new int[3];
+		B = new int[3];
+	}
+	
+	public void increment(char player) {
+		
+	}
+	
+	public String printScore() {
+		return null;
+	}
+	
+	public void parse(String order) {
+		
+	}
 }
